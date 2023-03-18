7b071d44bdf01cbbba4d13650e814c496a92cc1e
Andre Muniz <alnmuniz@gmail.com>
2023-03-18 17:55:51 -0300

    [ecore 009] Removing unused import.

--- src/test/java/com/ecore/roles/service/TeamsServiceTest.java
+++ src/test/java/com/ecore/roles/service/TeamsServiceTest.java
@@ -22,7 +22,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
-import java.util.Optional;
 import java.util.UUID;
 
 @ExtendWith(MockitoExtension.class)
