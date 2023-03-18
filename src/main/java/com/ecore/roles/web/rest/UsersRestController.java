d990a388b32344a638cb0d7fe418c0a1e6617f27
Andre Muniz <alnmuniz@gmail.com>
2023-03-18 13:58:25 -0300

    [ecore 005] Fix UsersRestController
    	- refactor 'produces = "application/json"' annotation parameter do
    class level.
    	- fix HTTP method api getUsers from POST do GET, according to restful
    pattern.
    	- fix HTTP method api getUser from POST do GET, according to restful
    pattern.

--- src/main/java/com/ecore/roles/web/rest/UsersRestController.java
+++ src/main/java/com/ecore/roles/web/rest/UsersRestController.java
@@ -1,31 +1,32 @@
 package com.ecore.roles.web.rest;
 
-import com.ecore.roles.service.UsersService;
-import com.ecore.roles.web.UsersApi;
-import com.ecore.roles.web.dto.UserDto;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import static com.ecore.roles.web.dto.UserDto.fromModel;
 
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
-import static com.ecore.roles.web.dto.UserDto.fromModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ecore.roles.service.UsersService;
+import com.ecore.roles.web.UsersApi;
+import com.ecore.roles.web.dto.UserDto;
+
+import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping(value = "/v1/users")
+@RequestMapping(value = "/v1/users", produces = {"application/json"})
 public class UsersRestController implements UsersApi {
 
     private final UsersService usersService;
 
     @Override
-    @PostMapping(
-            produces = {"application/json"})
+    @GetMapping()
     public ResponseEntity<List<UserDto>> getUsers() {
         return ResponseEntity
                 .status(200)
@@ -35,9 +36,8 @@ public class UsersRestController implements UsersApi {
     }
 
     @Override
-    @PostMapping(
-            path = "/{userId}",
-            produces = {"application/json"})
+    @GetMapping(
+            path = "/{userId}")
     public ResponseEntity<UserDto> getUser(
             @PathVariable UUID userId) {
         return ResponseEntity
